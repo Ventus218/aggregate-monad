6937f8a5faedd312e859ca182f8ebc80f658974b
Alessandro Venturini <ales.ventus21@gmail.com>
2025-12-16 16:07:15 +0100

    Initial commit

---
+++ src/main/scala/AggregateSemantic.scala
@@ -0,0 +1,105 @@
+import AggregateGrammar.*
+
+type Device = Int
+case class Env(nvalues: Map[Device, ValueTree[Any]], self: Device)
+
+enum ValueTree[A]:
+  // cond = None -> root
+  case Branch(cond: Option[Boolean], children: Seq[ValueTree[A]])
+  case Exchange(send: NValue[A])
+
+object ValueTree:
+  def empty[A]: ValueTree[A] = ValueTree.Branch(None, Seq())
+
+import cats.data.State
+import cats.data.ReaderT
+type ValueTreeState[A] = State[ValueTree[Any], A]
+type Round[A] = ReaderT[ValueTreeState, Env, A]
+import cats.~>
+def compiler: AggregateGrammar ~> Round =
+  new (AggregateGrammar ~> Round):
+    def apply[A](fa: AggregateGrammar[A]): Round[A] =
+      fa match
+        case Exchange(init, body) =>
+          for
+            env <- ReaderT.ask[ValueTreeState, Env]
+            (ret, send) = body
+              .asInstanceOf[NValue[Any] => (A, NValue[Any])](env.nvalues)
+            _ <- ReaderT.liftF(
+              State.modify[ValueTree[Any]](curr =>
+                curr match
+                  case ValueTree.Branch(cond, vts) =>
+                    ValueTree.Branch(cond, vts :+ ValueTree.Exchange(send))
+                  case ValueTree.Exchange(_) =>
+                    throw new IllegalStateException()
+              )
+            )
+          yield (ret)
+        case Branch(cond, the, els) =>
+          for
+            env <- ReaderT.ask[ValueTreeState, Env]
+            branch = if cond then the else els
+            (vt, ret) = branch
+              .foldMap(compiler)
+              .run(env)
+              .run(ValueTree.Branch(Some(cond), Seq()))
+              .value
+            _ <- ReaderT.liftF(
+              State.modify[ValueTree[Any]](curr =>
+                curr match
+                  case ValueTree.Branch(c, vts) =>
+                    ValueTree.Branch(c, vts :+ vt)
+                  case ValueTree.Exchange(_) =>
+                    throw new IllegalStateException()
+              )
+            )
+          yield (ret)
+
+@main
+def p: Unit =
+  val program =
+    for
+      _ <- exchange(0, _ => retsend(0))
+      _ <- exchange(0, _ => retsend(0))
+      res <- branch(
+        true,
+        for
+          _ <- exchange(0, _ => retsend(0))
+          _ <- branch(
+            true,
+            for
+              _ <- exchange(0, _ => retsend(0))
+              _ <- exchange(0, _ => retsend(0))
+            yield (),
+            exchange(0, _ => retsend(0))
+          )
+        yield (),
+        exchange(0, _ => retsend(0))
+      )
+    yield (res)
+  val round = program.foldMap(compiler)
+  val env = Env(Map(), 3)
+  println(round(env).run(ValueTree.empty).value)
+
+  val program2 =
+    for
+      _ <- exchange(0, _ => retsend(0))
+      _ <- exchange(0, _ => retsend(0))
+      res <- branch(
+        false,
+        for
+          _ <- exchange(0, _ => retsend(0))
+          _ <- branch(
+            true,
+            for
+              _ <- exchange(0, _ => retsend(0))
+              _ <- exchange(0, _ => retsend(0))
+            yield (),
+            exchange(0, _ => retsend(0))
+          )
+        yield (),
+        exchange(0, _ => retsend(0))
+      )
+    yield (res)
+  val round2 = program2.foldMap(compiler)
+  println(round2(env).run(ValueTree.empty).value)
