4ff3cc53f3d11707a988f9bda68d2cc4f7add364
Alessandro Venturini <ales.ventus21@gmail.com>
2026-01-06 12:20:56 +0100

    trying to isolate the alignment effect

--- src/main/scala/AggregateAPI.scala
+++ src/main/scala/AggregateAPI.scala
@@ -1,5 +1,147 @@
 package aggregate
 
+object AggregateAlignment:
+  enum AlignmentGrammar[A]:
+    case Enter(name: String) extends AlignmentGrammar[Unit]
+    case Exit() extends AlignmentGrammar[Unit]
+
+  import cats.free.Free
+
+  type Alignment[A] = Free[AlignmentGrammar, A]
+
+  def enter(name: String) =
+    Free.liftF(AlignmentGrammar.Enter(name))
+  def exit =
+    Free.liftF(AlignmentGrammar.Exit())
+
+  import cats.~>
+  import cats.data.State
+
+  enum OrderedTree[+A]:
+    case Empty
+    case Node(value: A, child: OrderedTree[A], sibling: OrderedTree[A])
+    override def toString(): String =
+      this match
+        case Empty => "_"
+        case Node(value, child, sibling) =>
+          s"Node($value, ${child.toString()}, ${sibling.toString()})"
+  object OrderedTree:
+    extension [A](t: OrderedTree[A])
+      /** Expects a vaild path for appending (not inserting) */
+      def appendSiblingAtPath(s: OrderedTree[A], path: Path): OrderedTree[A] =
+        t match
+          case Empty =>
+            require(path.isEmpty, "path doesn't match the tree")
+            s
+          case Node(value, child, sibling) =>
+            path match
+              case Nil =>
+                Node(value, child, sibling.appendSiblingAtPath(s, Nil))
+              case Choice.Child :: t =>
+                Node(value, child.appendSiblingAtPath(s, t), sibling)
+              case Choice.Sibling :: t =>
+                Node(value, child, sibling.appendSiblingAtPath(s, t))
+
+      /** Expects a vaild path for appending (not inserting) */
+      def appendChildAtPath(c: OrderedTree[A], path: Path): OrderedTree[A] =
+        t match
+          case Empty =>
+            throw IllegalArgumentException(
+              "Can't append a child to an Empty tree"
+            )
+          case Node(value, child, sibling) =>
+            path match
+              case Nil =>
+                Node(value, child.appendSiblingAtPath(c, Nil), sibling)
+              case Choice.Child :: t =>
+                Node(value, child.appendChildAtPath(c, t), sibling)
+              case Choice.Sibling :: t =>
+                Node(value, child, sibling.appendChildAtPath(c, t))
+
+      def subtreeAtPath(path: Path): OrderedTree[A] =
+        t match
+          case Empty =>
+            require(path.isEmpty, "path doesn't match the tree")
+            Empty
+          case Node(value, child, sibling) =>
+            path match
+              case Nil                 => t
+              case Choice.Child :: t   => child.subtreeAtPath(t)
+              case Choice.Sibling :: t => sibling.subtreeAtPath(t)
+
+      def children: Seq[OrderedTree[A]] =
+        t match
+          case Empty             => Seq()
+          case Node(_, Empty, _) => Seq()
+          case Node(_, child, _) => Seq(child) ++ child.siblings
+
+      def siblings: Seq[OrderedTree[A]] =
+        t match
+          case Empty               => Seq()
+          case Node(_, _, Empty)   => Seq()
+          case Node(_, _, sibling) => Seq(sibling) ++ sibling.siblings
+
+  enum Choice:
+    case Child
+    case Sibling
+
+  type Path = List[Choice]
+
+  case class AlignmentState(tree: OrderedTree[Any], path: Path)
+  type TreeState[A] = State[AlignmentState, A]
+
+  object TreeState:
+    import OrderedTree.*
+    import Choice.*
+    def enter(name: String): TreeState[Unit] =
+      State.modify(s =>
+        val newTree = s.tree.appendChildAtPath(Node(name, Empty, Empty), s.path)
+        val newPath = (s.path :+ Child) ++ newTree
+          .subtreeAtPath(s.path)
+          .children
+          .drop(1)
+          .foldLeft(List.empty[Choice])((p, _) => p :+ Sibling)
+        AlignmentState(newTree, newPath)
+      )
+    def exit: TreeState[Unit] =
+      State.modify(s =>
+        val newPath = s.path.reverse.dropWhile(_ == Sibling).drop(1).reverse
+        s.copy(path = newPath)
+      )
+
+  def compiler: AlignmentGrammar ~> TreeState =
+    new (AlignmentGrammar ~> TreeState):
+      def apply[A](fa: AlignmentGrammar[A]): TreeState[A] =
+        import AlignmentGrammar.*
+        fa match
+          case Enter(name) => TreeState.enter(name)
+          case Exit()      => TreeState.exit
+
+  @main
+  def main: Unit =
+    val prog = for
+      _ <- enter("sos")
+      _ <- enter("sis")
+      _ <- enter("sus")
+      _ <- exit
+      _ <- enter("yey")
+      _ <- exit
+      _ <- enter("yoy")
+      _ <- enter("yoy")
+    yield ()
+
+    println:
+      prog
+        .foldMap(compiler)
+        .run(
+          AlignmentState(
+            OrderedTree.Node("root", OrderedTree.Empty, OrderedTree.Empty),
+            Nil
+          )
+        )
+        .value
+        ._1
+
 trait AggregateAPI:
   type Device
   type Aggregate[_]
