4265575f822c8d1fdb8466fb0c5c16af35e06ff1
Alessandro Venturini <ales.ventus21@gmail.com>
2026-01-28 14:56:14 +0100

    add gradient, broadcase, distanceBetween, channel to Lib

--- src/main/scala/AggregateLib.scala
+++ src/main/scala/AggregateLib.scala
@@ -68,6 +68,48 @@ object AggregateLib:
         uidNV <- uid
       yield uidNV.map(uidValue => aNV(uidValue))
 
+  def gradient(
+      src: Aggregate[Boolean]
+  )(using range: Aggregate[Double]): Aggregate[Double] =
+    import scala.language.implicitConversions
+    exchange(Double.PositiveInfinity): v =>
+      retsend:
+        mux(src)(0.0):
+          nfold(Double.PositiveInfinity)(range + v)(_ min _)
+
+  def broadcast[A](src: Aggregate[Boolean])(field: Aggregate[A])(using
+      range: Aggregate[Double]
+  ): Aggregate[(Double, A)] =
+    import scala.language.implicitConversions
+    extension [A](a: Aggregate[A])
+      infix def ->[B](b: Aggregate[B]): Aggregate[(A, B)] =
+        pointwise(a, b, (a, b) => (a, b))
+    extension [A, B](a: Aggregate[(A, B)])
+      def _1: Aggregate[A] = a.map(_.map(_._1))
+      def _2: Aggregate[B] = a.map(_.map(_._2))
+    extension [A](dv: (Double, A))
+      infix def min(dv2: (Double, A)): (Double, A) =
+        if dv._1 < dv2._1 then dv else dv2
+
+    exchange(Double.PositiveInfinity -> field): dv =>
+      retsend:
+        mux(src)(0.0 -> field):
+          val dv1PlusRange = dv._1 + range
+          val tuple = dv1PlusRange -> dv._2
+          nfold(Double.PositiveInfinity -> field)(tuple)(_ min _)
+
+  def distanceBetween(src: Aggregate[Boolean], dest: Aggregate[Boolean])(using
+      range: Aggregate[Double]
+  ): Aggregate[Double] =
+    broadcast(src)(gradient(dest)).map(_.map(_._2))
+
+  def channel(
+      src: Aggregate[Boolean],
+      dest: Aggregate[Boolean],
+      width: Aggregate[Double]
+  )(using range: Aggregate[Double]): Aggregate[Boolean] =
+    gradient(src) + gradient(dest) - distanceBetween(src, dest) < width
+
   // Equals
   extension [A](a: Aggregate[A])
     infix def eq(b: Aggregate[A]): Aggregate[Boolean] =
